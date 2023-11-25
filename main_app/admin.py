bf410c145696572afe480ce16f17b0586e919387
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2023-11-25 14:38:04 +0300

    added models and registered app_models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
+from main_app.models import User, Property, Review, Message, Appointment, Payment
+
 # Register your models here.
+admin.site.site_header = "Dwell Hub System"
+admin.site.index_title = "Dwell Hub"
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'user_type')
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'landlord', 'price', 'availability')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'property', 'rating', 'timestamp')
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'receiver', 'timestamp')
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'property', 'appointment_date', 'appointment_time', 'status')
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'property', 'amount', 'payment_date', 'payment_method', 'status')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(Payment, PaymentAdmin)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=50)
+    profile_picture = models.ImageField(upload_to='', blank=True, null=True)
+    phone = models.CharField(max_length=100)
+    reg_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Property(models.Model):
+    landlord = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    message_content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sender
+
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    appointment_date = models.DateField()
+    appointment_time = models.TimeField()
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=50)  # e.g., 'M-Pesa'
+    status = models.CharField(max_length=20)  # e.g., 'pending', 'completed', 'refunded'
+
+    def __str__(self):
+        return self.user
